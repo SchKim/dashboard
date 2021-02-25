ddab8b92cb9a0ef25311ab9419bcbe00d1943617
Kim Schenk <kimberly.schenk@rabobank.nl>
2021-02-25 21:39:41 +0100

    WIP background addes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import './App.css';
+import styled from 'styled-components';
+import backgroundImg from './shared/assets/backgroundImg.jpg';
+
 import MenuComponent from './components/MenuComponent';
+
+const BackgroundImg = styled.img`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  /* background: url('http://www.baltana.com/files/wallpapers-16/Digital-Art-Background-HD-Wallpapers-41706.jpg') */
+  /* center / cover no-repeat; */
+  z-index: -1;
+`;
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>boe</header>
+      <BackgroundImg src={backgroundImg}></BackgroundImg>
       <MenuComponent />
     </div>
   );
