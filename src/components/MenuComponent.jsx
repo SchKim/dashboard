dd3aadd21927f34263f39db69c7591fe88d9ad7b
Kim schenk <k.schenk@belastingdienst.nl>
2021-03-09 15:59:25 +0100

    menu 4.0

--- src/components/MenuComponent.jsx
+++ src/components/MenuComponent.jsx
@@ -60,84 +60,60 @@ export default function MenuComponent() {
 const Menu = styled.nav`
   background: #f97970;
   position: absolute;
-    left:0;
-    bottom:0;
-    right:0;
+  left: 0;
+  bottom: 0;
+  right: 0;
 
   ul {
     display: grid;
     grid-template-columns: repeat(5, 1fr);
     place-items: center center;
     padding-inline-start: 0px;
-    line-height:20px
   }
 
   li {
     list-style-type: none;
-    
-  }
- 
-
-  /* li:hover::after {
-    position: absolute;
-    top: -125px;
-    left: 7%;
-    padding: 10px;
-    border-radius: 50px;
-    font: oblique small-caps bold 30px/21px Georgia, serif;
-    background: #fff;
-  }
-  li:nth-child(2):hover::after {
-    content: "Karakter";
-    top: -75px;
-    left: 5%;
-  }
-  li:nth-child(3):hover::after {
-    content: "Knowledge Book";
-    top: -75px;
-    left: 25%;
-  }
-  li:nth-child(4):hover::after {
-    content: "Achievements";
-    top: -75px;
-    left: 45%;
-  }
-  li:nth-child(5):hover::after {
-    content: "Quests";
-    top: -75px;
-    left: 65%;
-  }
-  li:nth-child(6):hover::after {
-    content: "History";
-    top: -75px;
-    left: 85%; */
-
-    /* @media only screen and (max-width: 900px) {
-      left: 81%;
-    }
-    @media only screen and (max-width: 600px) {
-      left: 73%;
-    } */
   }
 
+  ul li a {
+    color: #f97970;
+    font-size: 24px;
+    position: relative;
+  }
   /* top en lest weg werken met relastive aan parent  */
 
- /* op de hover display no  */
+  /* op de hover display no  */
+
+  li a > i {
+    font-size: 45px;
+    line-height: 45px;
+    color: #fbefd7;
+    width: 100%;
+  }
+  li a:focus > i,
+  ul li a:hover i,
+  ul li i:hover {
+    color: #02132c;
+  }
 
   .menu__lastitem {
     grid-column-end: -1;
   }
-  li a > i {
-    font-size: 45px;
-    line-height:45px;
-    color: #fbefd7;
-    width: 100%;
+  ul li a:hover div {
+    display: block;
   }
-  li a > i:hover, li a:focus > i {
-    color: #02132c;
-  }
-
- 
 `;
-
-const Tooltip = styled.div``;
+const Tooltip = styled.div`
+  position: absolute;
+  top: -80px;
+  left: -20px;
+  background: #fff;
+  width: 100%px;
+  padding: 10px;
+  border-radius: 50px;
+  text-transform: uppercase;
+  font-size: 10px;
+  letter-spacing: 3px;
+  font-weight: bold;
+  display: none;
+`;
