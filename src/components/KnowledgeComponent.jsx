a6f920c519f9d2c5c51cca8d36075d656a31fd1a
Kim schenk <k.schenk@belastingdienst.nl>
2021-04-24 20:11:23 +0200

    styling own file

--- src/components/KnowledgeComponent.jsx
+++ src/components/KnowledgeComponent.jsx
@@ -1,62 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 import CharacterJson from "../shared/api.json";
+import { FaGlasses } from "react-icons/fa";
 
-const KnowledgeWrapper = styled.div`
-  position: absolute;
-  left: 40px;
-  top: 40px;
-  background: #fff;
-  border-radius: 50px;
-  padding: 30px;
-  margin: 30px 0 0 0;
-  max-width: 1000px;
-  width: 50%;
-
-  @media only screen and (max-width: 1600px) {
-    width: 90%;
-  }
-
-  @media only screen and (max-width: 900px) {
-    width: 70%;
-    left: 7%;
-    top: 20px;
-  }
-  @media only screen and (max-width: 500px) {
-    width: 100%;
-    left: 0;
-  }
-`;
-const KnowledgeDiv = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-  align-items: center;
-`;
-const KnowledgeTitle = styled.h1`
-  display: list-item;
-  content: " ";
-
-  ::marker {
-    color: #f97970;
-  }
-`;
-
-const KnowledgeIcon = styled.i`
-  font-size: 25px;
-  padding: 3%;
-  pointer-events: none;
-  color: #f97970;
-`;
-
-const KnowledgeRevealer = styled.div`
-  display: none;
-`;
-
-const KnowledgeTextWrapper = styled.div`
-  max-width: 80%;
-  padding: 10%;
-`;
+import {
+  KnowledgeWrapper,
+  KnowledgeDiv,
+  KnowledgeTitle,
+  KnowledgeIcon,
+  KnowledgeRevealer,
+  KnowledgeTextWrapper,
+} from "./styles/styles";
+function clickHandlerKarakter(id) {}
 
 export default function KnowledgeComponent() {
   return (
@@ -64,8 +19,8 @@ export default function KnowledgeComponent() {
       <KnowledgeDiv>
         {CharacterJson.map((knowledge) => (
           <div key={knowledge.id}>
-            <button>
-              <KnowledgeIcon className={knowledge.symbol}></KnowledgeIcon>
+            <button onClick={clickHandlerKarakter(knowledge.id)}>
+              <KnowledgeIcon>{knowledge.symbol}</KnowledgeIcon>
             </button>
             <KnowledgeRevealer>
               <KnowledgeTitle>{knowledge.title}</KnowledgeTitle>
@@ -74,7 +29,11 @@ export default function KnowledgeComponent() {
           </div>
         ))}
       </KnowledgeDiv>
-      <KnowledgeTextWrapper>hier komt de text</KnowledgeTextWrapper>
+      <KnowledgeTextWrapper>
+        hier komt de text <FaGlasses />
+      </KnowledgeTextWrapper>
     </KnowledgeWrapper>
   );
 }
+
+// react FONTAwesome
---
+++ src/components/styles/styles.js
@@ -0,0 +1,58 @@
+// import styled, { css } from "styled-components";
+import styled from "styled-components";
+
+export const KnowledgeWrapper = styled.div`
+  position: absolute;
+  left: 40px;
+  top: 40px;
+  background: #fff;
+  border-radius: 50px;
+  padding: 30px;
+  margin: 30px 0 0 0;
+  max-width: 1000px;
+  width: 50%;
+
+  @media only screen and (max-width: 1600px) {
+    width: 90%;
+  }
+
+  @media only screen and (max-width: 900px) {
+    width: 70%;
+    left: 7%;
+    top: 20px;
+  }
+  @media only screen and (max-width: 500px) {
+    width: 100%;
+    left: 0;
+  }
+`;
+export const KnowledgeDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const KnowledgeTitle = styled.h1`
+  display: list-item;
+  content: " ";
+
+  ::marker {
+    color: #f97970;
+  }
+`;
+
+export const KnowledgeIcon = styled.i`
+  font-size: 25px;
+  padding: 3%;
+  pointer-events: none;
+  color: #f97970;
+`;
+
+export const KnowledgeRevealer = styled.div`
+  display: none;
+`;
+
+export const KnowledgeTextWrapper = styled.div`
+  max-width: 80%;
+  padding: 10%;
+`;
