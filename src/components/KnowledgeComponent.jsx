1db4d5d67dde5c81735c773d063c057603bf65b2
Kim schenk <k.schenk@belastingdienst.nl>
2021-05-15 23:16:12 +0200

    WIP: speelt af maar 2 is moeilijk

--- src/components/KnowledgeComponent.jsx
+++ src/components/KnowledgeComponent.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Typing from "react-typing-animation";
 import CharacterJson from "../shared/api.json";
 import {
   FaAddressCard,
@@ -20,28 +19,42 @@ import {
   KnowledgeTextWrapper,
 } from "./styles/styles";
 
+/// naar werkende versie
+
 export default function KnowledgeComponent() {
+  const [fullText, setFullText] = useState("");
+  const [index, setIndex] = useState(0);
   const [description, setDescription] = useState("");
 
-  //probleem : discription word geladen.. en typing ... maar blijft lopen en plakt alles aan elkaar vast
-  // als ik op een btn click wil ik dat die stopt en verder gaat met een ander
+  //handel click vangt de waarde en vuly die waar de bij setFulltext => fulltext
+  const handleClick = (description) => {
+    // setFullText("");
+    // setIndex(() => 0);
+    // setDescription("");
 
-  // use effe dependencie description
+    setFullText(description);
+  };
 
   useEffect(() => {
-    console.log("is veranderd");
-    const test = description;
-    setDescription("");
-    console.log(test);
-    //setDescription();
-  }, []);
+    // index staat op 0 als fulltext kleinder is
+    if (index < fullText.length) {
+      //set setTimeout regelt de vertraging
+      setTimeout(() => {
+        //  set description wordt gevuld en ook met de huidige waarde daarvan / fulltext is de zin en de index loopt daar hoorheen hierna verhoogd met +1
+        setDescription(description + fullText[index]);
+        setIndex(index + 1);
+      }, 70);
+    }
+    // in de dependencie wil ik allen de index en fulltext omdat description 2 x renderd en dat trillingen geeft
+  }, [index, fullText]);
 
+  //TODO probleen is dat als use effect eenmaal loopt .. die nioet meer stopt als in op en andere btn druk
   return (
     <KnowledgeWrapper>
       <KnowledgeDiv>
         {CharacterJson.map((knowledge) => (
           <div key={knowledge.id}>
-            <button onClick={() => setDescription(knowledge.description)}>
+            <button onClick={() => handleClick(knowledge.description)}>
               <KnowledgeIcon>{getIcon(knowledge.symbol)}</KnowledgeIcon>
             </button>
           </div>
@@ -49,9 +62,7 @@ export default function KnowledgeComponent() {
       </KnowledgeDiv>
 
       {description && (
-        <KnowledgeTextWrapper>
-          <Typing>{description}</Typing>
-        </KnowledgeTextWrapper>
+        <KnowledgeTextWrapper>{description}</KnowledgeTextWrapper>
       )}
     </KnowledgeWrapper>
   );
